1f9ab49832bd30d4eef2ba987e54353028440a87
Paloma Souza <palomansf@outlook.com>
2026-08-04 20:19:17 -0300

    Adiciona funcionamento da calculadora

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Calculadora iniciada!")
+let visor = document.querySelector(".visor");
+let botoes = document.querySelectorAll("button");
+
+let valorAtual = "";
+
+botoes.forEach(function(botao) {
+
+    botao.addEventListener("click", function() {
+
+        let valorBotao = botao.innerText;
+
+        if (valorBotao === "=") {
+
+            visor.value = eval(valorAtual);
+
+        } else if (valorBotao === "C") {
+
+            valorAtual = "";
+            visor.value = "0";
+
+        } else {
+
+            valorAtual += valorBotao;
+            visor.value = valorAtual;
+
+        }
+
+    });
+
+});
